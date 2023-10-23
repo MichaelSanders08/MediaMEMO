344a478b56f446243505a46580041909158bc6e5
Ethan Schmidt <eschmidt16@huskers.unl.edu>
2023-10-23 14:23:42 -0500

    Updated list message contents

--- src/data/messages.ts
+++ src/data/messages.ts
@@ -1,59 +1,54 @@
 export interface Message {
-  fromName: string;
-  subject: string;
+  title: string;
+  genres: string;
   date: string;
+  description: string;
   id: number;
 }
 
 const messages: Message[] = [
   {
-    fromName: 'Matt Chorsey',
-    subject: 'New event: Trip to Vegas',
-    date: '9:32 AM',
+    title: 'The Domino Revival',
+    genres: 'Documentary',
+    date: '10/24/2023',
+    description: "The Domino Revival takes moviegoers on an extraordinary journey with Mike Signorelli and a group of revivalists during a pivotal period in our nation's history. As society's fascination with the supernatural intensifies, this film unveils the awe-inspiring power of Jesus Christ. Through compelling Gospel preaching, documented miracles, triumph over despair, and liberation from demons, The Domino Revival captures the essence of spiritual hunger and delivers a profound cinematic experience.",
     id: 0
   },
   {
-    fromName: 'Lauren Ruthford',
-    subject: 'Long time no chat',
-    date: '6:12 AM',
+    title: "Five Nights at Freddy's",
+    genres: 'Horror | Mystery | Thriller',
+    date: '10/27/2023',
+    description: "A troubled security guard begins working at Freddy Fazbear's Pizza. During his first night on the job, he realizes that the night shift won't be so easy to get through. Pretty soon he will unveil what actually happened at Freddy's.",
     id: 1
   },
   {
-    fromName: 'Jordan Firth',
-    subject: 'Report Results',
-    date: '4:55 AM',
+    title: 'Freelance',
+    genres: 'Action | Comedy',
+    date: '10/27/2023',
+    description: 'An ex-special forces operative takes a job to provide security for a journalist as she interviews a dictator, but, a military coup breaks out in the middle of the interview, they are forced to escape into the jungle where they must survive',
     id: 2
 
   },
   {
-    fromName: 'Bill Thomas',
-    subject: 'The situation',
-    date: 'Yesterday',
+    title: 'After Death',
+    genres: 'Documentary',
+    date: '10/27/2023',
+    description: 'Based on real near-death experiences, After Death explores the afterlife with the guidance of New York Times bestselling authors, medical experts, scientists, and survivors that shed a light on what awaits us.',
     id: 3
   },
   {
-    fromName: 'Joanne Pollan',
-    subject: 'Updated invitation: Swim lessons',
-    date: 'Yesterday',
+    title: 'Suitable Flesh',
+    genres: 'Horror | Thriller',
+    date: '10/27/2023',
+    description: 'A psychiatrist becomes obsessed with one of her young clients with multiple personalities.',
     id: 4
   },
   {
-    fromName: 'Andrea Cornerston',
-    subject: 'Last minute ask',
-    date: 'Yesterday',
+    title: 'Inspector Sun',
+    genres: 'Animation | Comedy | Family',
+    date: '10/27/2023',
+    description: "Inspector Sun boards a seemingly normal plane for a much-needed vacation, but when an enigmatic millionaire receives a threat on his life, Sun is back on the case. Trapped in a web of lies, Sun must find the culprit before it's too late.",
     id: 5
-  },
-  {
-    fromName: 'Moe Chamont',
-    subject: 'Family Calendar - Version 1',
-    date: 'Last Week',
-    id: 6
-  },
-  {
-    fromName: 'Kelly Richardson',
-    subject: 'Placeholder Headhots',
-    date: 'Last Week',
-    id: 7
   }
 ];
 
