936e8ff2519af65ee854687f8a5e926de478e0e8
MichaelSanders08 <59788429+MichaelSanders08@users.noreply.github.com>
2023-10-27 00:05:07 -0500

    added setting button fixed top bar
    Made the top bars uniform across pages. In the top bars added a setting button and icon to them as well.

--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -1,10 +1,10 @@
 <style scoped>
-  .example-content {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100%;
-  }
+.example-content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
 </style>
 
 <template>
@@ -12,18 +12,54 @@
     <ion-header>
       <ion-toolbar>
         <ion-title>Search</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content>
-      <div class="example-content">Search content</div>
-    </ion-content>
+        <ion-button color="transparent" slot="end">
+          <ion-icon
+            slot="icon-only"
+            :icon="cog"
+            style="color: black"
+          ></ion-icon> </ion-button></ion-toolbar
+    ></ion-header>
+    <ion-toolbar>
+      <ion-searchbar
+        style="padding-top: 15px"
+        v-model="query"
+        @ionChange="handleSearch"
+        placeholder="Enter Media"
+        animated="true"
+      ></ion-searchbar>
+    </ion-toolbar>
+
+    <ion-content> </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-  import { IonHeader, IonToolbar, IonTitle, IonContent, IonPage } from '@ionic/vue';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonPage,
+} from "@ionic/vue";
+import { cog } from "ionicons/icons";
 
-  export default {
-    components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
-  };
+export default {
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+  data() {
+    return {
+      query: "",
+    };
+  },
+  setup() {
+    return {
+      cog,
+    };
+  },
+  methods: {
+    handleSearch() {
+      // Your search logic here
+      console.log("Searching for:", this.query);
+    },
+  },
+};
 </script>
