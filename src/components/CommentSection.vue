f12a55b92fd78015f268133b52ad9195d075522e
MichaelSanders08 <59788429+MichaelSanders08@users.noreply.github.com>
2023-12-06 02:40:49 -0600

    added comment section
    Co-Authored-By: John Le <69764817+Johonel@users.noreply.github.com>
    Co-Authored-By: Ethan Schmidt <69820496+eschmiddy16@users.noreply.github.com>

---
+++ src/components/CommentSection.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="comment-section">
+    <h2>Comments</h2>
+
+    <ion-list>
+      <ion-item v-for="comment in comments">
+        <div class="header">
+          <h2 class="author">{{ comment.author }}</h2>
+          <IonNote> {{ comment.timestamp }}</IonNote>
+
+          <h3 class="comment-text">
+            {{ comment.text }}
+          </h3>
+        </div>
+      </ion-item>
+    </ion-list>
+
+    <form class="textform" @submit.prevent="submitComment">
+      <textarea
+        placeholder="Leave a comment..."
+        v-model="newComment"
+      ></textarea>
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { IonNote } from "@ionic/vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      newComment: "",
+      comments: [] as Array<{
+        author: string;
+        text: string;
+        timestamp: string;
+      }>, // Define the type for comments array
+    };
+  },
+  methods: {
+    submitComment() {
+      if (this.newComment.trim()) {
+        this.comments.push({
+          author: "Current User",
+          text: this.newComment,
+          timestamp: new Date().toISOString(),
+        });
+        this.newComment = "";
+        console.log("PRINTING A COMMENT");
+      }
+    },
+  },
+  components: { IonNote },
+});
+</script>
+
+<style scoped>
+.comment-section {
+  padding: 16px;
+  border-top: 1px solid #ddd;
+}
+
+.comment-section h2 {
+  margin-bottom: 8px;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+li {
+  margin-bottom: 8px;
+}
+
+strong {
+  font-weight: bold;
+}
+
+.timestamp {
+  color: gray;
+  font-size: 12px;
+}
+
+textarea {
+  width: 100%;
+  padding: 8px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  resize: none;
+}
+
+button {
+  padding: 8px 16px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  background-color: #fff;
+  cursor: pointer;
+}
+.textform {
+  margin-top: 20px;
+}
+.author {
+  font-weight: bold;
+}
+</style>
